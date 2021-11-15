7de77c74cc5391a0da8be7c12d725f04c2636aa6
Juan Garcia <juanos0808@gmail.com>
2021-11-15 18:04:18 +0100

    * Login + Notas

--- routes/notas.routes.js
+++ routes/notas.routes.js
@@ -1,17 +1,25 @@
 const { Router } = require('express');
 const {addNote, obtainNota, obtainNotas, deleteNota, updateNota} = require("../controllers/notas.controller");
-const {fieldvalidator} = require("../middlewares");
+const {fieldvalidator, jwtValidator, isAdminRole} = require("../middlewares");
 
 
 const router = Router();
 
-router.post('/nueva-nota', addNote);
+router.post('/nueva-nota', [
+    jwtValidator,
+    isAdminRole,
+    fieldvalidator
+],addNote);
 
 router.get('/:id', [
+    jwtValidator,
     fieldvalidator
 ],obtainNota);
 
-router.get('/',obtainNotas);
+router.get('/',[
+    jwtValidator,
+    fieldvalidator
+],obtainNotas);
 
 router.delete('/:id',deleteNota);
 
